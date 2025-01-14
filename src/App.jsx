fe43ffbdcc8a3aa084aba4c33a1ba6a045154d99
bilal <mail2muhammedbilal@gmail.com>
2025-01-15 01:00:38 +0530

    feat: child keyboard input container update

--- src/App.jsx
+++ src/App.jsx
@@ -41,11 +41,13 @@ function App() {
   };
 
   const onClickBackSpace = () => {
-    // let filterCards = inputCards
-    // filterCards.pop()
-    // setInputCards([filterCards]);
-
     setInputCards((prevCards) => prevCards.slice(0, -1));
+  }
+
+  const onClickPlayAll = () => {
+    const message = inputCards.reduce((oldValue,newValue)=> oldValue + ' ' + newValue?.text,'')
+
+    handleTextToSpeech(message)
   }
 
   return (
@@ -59,10 +61,10 @@ function App() {
               ))
             }
           </div>
-          <div className="play-button-container d-flex v-h-center link">
+          <div className="play-button-container d-flex v-h-center link" title='Play all' onClick={() => onClickPlayAll()}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="m380-300 280-180-280-180v360ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z" /></svg>
           </div>
-          <div className="close-button-container d-flex v-h-center link" onClick={() => onClickBackSpace()}>
+          <div className="close-button-container d-flex v-h-center link" title='Backspace' onClick={() => onClickBackSpace()}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="m456-320 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 160q-19 0-36-8.5T296-192L80-480l216-288q11-15 28-23.5t36-8.5h440q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H360ZM180-480l180 240h440v-480H360L180-480Zm400 0Z" /></svg>
           </div>
         </div>
